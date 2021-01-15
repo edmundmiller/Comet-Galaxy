ac5616d888a1a3a7c8d2873042e7ebae1f5da11c
kwakp05 <kwakp05@gmail.com>
2021-01-14 21:07:01 -0600

    Add first pass at 'tss' rule

--- promoter.pl
+++ promoter.pl
@@ -3,7 +3,6 @@
 % need new rules
 bacteria(_).
 eukaryote(_).
-% tss(S):-.
 
 promoter(G) :-
     % tss - transcription start site
@@ -36,6 +35,14 @@ tatabox(P) :- similar(P, ['t','a','t','a','a','a','t']).
 tatabox(P) :- similar(P, ['t','a','t','a','t','a','a']).
 tatabox(P) :- similar(P, ['t','a','t','a','t','a','t']).
 
+stop_codon(['t', 'a', 'g']).
+stop_codon(['t', 'a', 'a']).
+stop_codon(['t', 'g', 'a']).
+
+reversed_stop_codon(P) :-
+	stop_codon(Q),
+	reverse_list(Q, P).
+
 % Generate all O(N^2) sublists of the input list
 generate_sublists([Head | Tail], Result) :-
 	generate_prefixlists([Head | Tail], Result).
@@ -46,6 +53,22 @@ generate_sublists([_ | Tail], Result) :-
 generate_prefixlists([Head | _], [Head | []]).
 generate_prefixlists([Head | Tail], [Head | Result]) :-
 	generate_prefixlists(Tail, Result).	
+
+% Finds all transcription sections in the Genome.
+% Transcription sections should start with a start codon and end with a stop codon.
+% However, there should not be any stop codons in the 'body' of the section.
+tts(Genome, Candidate) :-
+	generate_sublists(Genome, Candidate),
+	reverse_list(Candidate, ReverseCandidate),
+	reversed_stop_codon(Stop1),
+	prefix_match(['a', 't', 'g'], Candidate),
+	reversed_stop_codon(Stop2),
+	prefix_match(Stop2, ReverseCandidate).
+
+reverse_list(InputList, OutputList) :- reverse_list(InputList, [], OutputList).
+
+reverse_list([], Acc, Acc).
+reverse_list([Head | Tail], Acc, Reversed) :- reverse_list(Tail, [Head | Acc], Reversed).  
 
 list_length( []     , L , L ) .
 list_length( [_|Xs] , T , L ) :-
@@ -75,6 +98,12 @@ subsequence(Pattern, [_ | Tail], Index) :-
         subsequence(Pattern, Tail, Index2),
         Index is Index2 + 1.
 
+% Searches for `Pattern` in `Sequence` but ignores the subsequence
+% if it begins at index 0
+subsequence_ignore0(Pattern, Sequence) :-
+	subsequence(Pattern, Sequence, Index),
+	Index > 0.
+
 % ?- prefix_match([a, b, c], [a, b, c, d, e]).
 % ?- prefix_match([a, e, c], [a, b, c, d, e]).
 
@@ -86,3 +115,12 @@ subsequence(Pattern, [_ | Tail], Index) :-
 % ?- similar([a, b, d, e], [a, b, d, e]).
 
 % ?- generate_sublists([a, b, c, d], X).
+
+% ?- reverse_list([a, b, c, d], R).
+
+% ?- subsequence_ignore0([a, b], [a, b, c, b], X).
+% ?- subsequence_ignore0([a, b], [a, b, a, b], X).
+
+% ?- tts(['a', 't', 'g', 't', 'a', 'g'], X).
+% ?- tts(['a', 't', 'g', 'c', 'c', 't', 'a', 'a', 't', 'a', 'g'], X). 
+% ?- tts(['a', 't', 'c', 'c', 't', 'a', 'a', 't', 'a', 'g'], X). 
