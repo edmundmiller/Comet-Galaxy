8ff0712c48faed6a3456699371e21f9ea63c1c90
Edmund Miller <edmund.a.miller@protonmail.com>
2021-01-15 10:40:30 -0600

    style: Run formaters

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ The AT-rich sequences rarely appear exactly as shown in the model. In fact, the 
 
 ## Quick Start
 
-``` sh
+```sh
 git clone https://github.com/Emiller88/Comet-Galaxy.git
 make
 ```
@@ -22,16 +22,19 @@ make
 ## Usage
 
 1. Clone the git repository to your local machine.
+
 ```
 git clone https://github.com/Emiller88/Comet-Galaxy.git
 ```
 
 2. Run the `data_to_list.py` script to generate input for the s(CASP) program. (Note: You can supply your own genome by replacing raw_input2.txt with the file name of any local text file)
+
 ```
 python data_to_list.py raw_input2.txt
 ```
 
 3. Run the analyzer!
+
 ```
 scasp promoter.pl
 ```
--- data_to_list.py
+++ data_to_list.py
@@ -3,19 +3,17 @@ import argparse
 parser = argparse.ArgumentParser(description="Convert raw text input to s(CASP) list.")
 parser.add_argument("fileName", help="Input file name", metavar="NAME", nargs=1)
 args = parser.parse_args()
-file_name = args.fileName[0];
+file_name = args.fileName[0]
 
-with open(file_name, 'r') as in_file:
+with open(file_name, "r") as in_file:
     in_str = in_file.read()
     in_str = in_str.lower()
-    in_str = in_str.replace('\n', '')
-    in_str = in_str.replace('\r', '')
-    in_str = in_str.replace(' ', '')
+    in_str = in_str.replace("\n", "")
+    in_str = in_str.replace("\r", "")
+    in_str = in_str.replace(" ", "")
     in_list = list(in_str)
 
-with open('list_input.pl', 'w') as out_file:
-    out_file.write('sequence(')
+with open("list_input.pl", "w") as out_file:
+    out_file.write("sequence(")
     out_file.write(in_list.__str__())
-    out_file.write(').')
-
-
+    out_file.write(").")
