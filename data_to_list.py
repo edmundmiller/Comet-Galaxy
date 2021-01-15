e1f11923aedff1230f5f38fa53e51ff0855c9642
Edmund Miller <edmund.a.miller@protonmail.com>
2021-01-15 11:13:47 -0600

    fix: Remove fasta hack

--- data_to_list.py
+++ data_to_list.py
@@ -6,10 +6,11 @@ args = parser.parse_args()
 file_name = args.fileName[0]
 
 with open(file_name, "r") as in_file:
-    while True:
-        line = in_file.readline()
-        if not line.startswith(">"):
-            break
+    # FIXME
+    # while True:
+    #     line = in_file.readline()
+    #     if not line.startswith(">"):
+    #         break
 
     in_str = in_file.read()
     in_str = in_str.lower()
